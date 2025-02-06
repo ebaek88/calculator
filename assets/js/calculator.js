625507e8a813af5fad17b539d32bf05d258004b6
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-02-05 23:57:11 -0700

    fix: edit class Calculator
    Added class fields and additional methods

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,6 +1,14 @@
 // operator and its corresponding function
 
 class Calculator {
+  static #numerical = "1234567890";
+  #op1;
+  #op2;
+  #op;
+  #stack;
+  #takeInput;
+  #digitInput;
+
   constructor() {
     this.operators = {
       "+": (a, b) => a + b,
@@ -9,18 +17,44 @@ class Calculator {
       "/": (a, b) => a / b,
     };
 
-    this.op1;
-    this.op2;
-    this.op;
+    this.#op1 = "";
+    this.#op2 = "";
+    this.#op = "";
+    this.#stack = [];
+    this.#takeInput = true;
+  }
+
+  // getters and setters
+  getTakeInput() {
+    return this.#takeInput;
+  }
+
+  setTakeInput(boolean) {
+    this.#takeInput = boolean;
+  }
+
+  getDigitInput() {
+    return this.#digitInput;
+  }
+
+  setDigitInput(value) {
+    if (Calculator.#numerical.includes(value)) {
+      this.#digitInput = value;
+    }
+  }
+
+  // take input for an operand
+  enterInput(operand) {
+    while (this.#takeInput) {}
   }
 
   operate(op1, op2, op) {
     try {
-      if(!operators[op]) {
+      if (!operators[op]) {
         throw new Error("The operator does not exist in this calculator.");
       }
       return operators[op](op1, op2);
-    } catch(e) {
+    } catch (e) {
       console.log(e.message);
     }
   }
